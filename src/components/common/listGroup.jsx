ddeca3acae8a3c5b6ed408e7c50579258d5c8602
J B <justinian.boghici@gmail.com>
2020-02-02 21:56:38 +0000

    display filtering items

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const ListGroup = () => {
-  return null;
+const ListGroup = props => {
+  const { items, textPropperty, valueProperty } = props;
+
+  return (
+    <ul className='list-group'>
+      {items.map(item => (
+        <li key={item[valueProperty]} className='list-group-item'>
+          {item[textPropperty]}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ListGroup;
