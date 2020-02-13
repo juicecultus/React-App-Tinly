8056de8a620853ce82e627f94b7dc582a6f0eaff
J B <justinian.boghici@gmail.com>
2020-02-13 14:37:05 +0000

    hide delete column conditionally

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import auth from '../services/authService';
 import Like from './common/like';
 import TableHeader from './common/tableHeader';
 import TableBody from './common/tableBody';
-import { Link } from 'react-router-dom';
 
 class MoviesTable extends Component {
   columns = [
@@ -19,19 +20,26 @@ class MoviesTable extends Component {
       content: movie => (
         <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
       )
-    },
-    {
-      key: 'delete',
-      content: movie => (
-        <button
-          onClick={() => this.props.onDelete(movie)}
-          className='btn btn-danger btn-sm'
-        >
-          Delete
-        </button>
-      )
     }
   ];
+
+  deleteColumn = {
+    key: 'delete',
+    content: movie => (
+      <button
+        onClick={() => this.props.onDelete(movie)}
+        className='btn btn-danger btn-sm'
+      >
+        Delete
+      </button>
+    )
+  };
+
+  constructor() {
+    super();
+    const user = auth.getCurrentUser();
+    if (user && user.isAdmin) this.columns.push(this.deleteColumn);
+  }
 
   render() {
     const { movies, onSort, sortColumn } = this.props;
