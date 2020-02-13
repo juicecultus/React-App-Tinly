949c494f5bd870c14b10e90362fb19d9c1c84810
J B <justinian.boghici@gmail.com>
2020-02-13 10:50:48 +0000

    handle login errors

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -19,8 +19,18 @@ class LoginForm extends Form {
   };
 
   doSubmit = async () => {
-    const { data } = this.state;
-    await login(data.username, data.password);
+    try {
+      const { data } = this.state;
+      const { data: jwt } = await login(data.username, data.password);
+      localStorage.setItem('token', 'jwt');
+      this.props.history.push('/');
+    } catch (ex) {
+      if (ex.response && ex.reponse.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
